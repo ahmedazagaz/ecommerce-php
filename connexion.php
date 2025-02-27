7e6b35e23f58b4bbbb6a29b0f262a136129941ba
ahmedazagaz <contactwithazagaz@gmail.com>
2025-02-27 04:46:00 +0100

    connexion

--- connexion.php
+++ connexion.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Connexion</title>
 </head>
 <body>
     
@@ -12,32 +12,39 @@
 
     <div class="container py-2">
         <?php
-        //Ajouter utilisateur 
-        if(isset($_POST['ajouter'])) {
+            if(isset($_POST['connexion'])) {
             $login = $_POST['login'];
             $pwd = $_POST['password'];
-           
+
             if(!empty($login) && !empty($pwd)) {
                 require_once 'include/database.php';
-                $date = date('Y-m-d');
-                var_dump( $date);
-               $sqlStat = $pdo->prepare('INSERT INTO utilisateur VALUES(null,?,?,?)');
-               $sqlStat->execute(([$login,$pwd,"$date"]));
-               header("Location:connexion.php");
-
-            } else {
+                $sqlStat = $pdo->prepare('select * from utilisateur where login=? and password=?');
+                $sqlStat->execute(([$login,$pwd]));
+                if($sqlStat->rowCount() >=1) {
+                    session_start();
+                    $_SESSION['utilisateur'] = $sqlStat->fetch();
+                    header("Location:admin.php");
+                } else {
+                    ?>
+                    <div class="alert alert-danger" role="alert">
+                    Login ou mot de passe incorrect
+                    </div>
+                    <?php
+                }
+            }else{
                 ?>
                 <div class="alert alert-danger" role="alert">
-                Veuillez remplir tous les champs 
-                </div>
+                      Login,password sont obligatoires
+                </div> 
                 <?php
-        } 
+        }
     }
-        ?>
-        
-    <form method="post">
 
-    <div class="col-sm-2 col-form-label mb-3 " >
+  ?>
+  <h4>Connexion</h4>
+  <form method="post">
+
+  <div class="col-sm-2 col-form-label mb-3 " >
     <label class="form-label">login</label>
     <input type="text" class="form-control" name="login">
   </div>
@@ -47,7 +54,7 @@
     <input type="password" class="form-control" name="password">
   </div>
 
-  <button type="submit" class="btn btn-primary" name="ajouter">Ajouter utulisateur</button>
+  <button type="submit" class="btn btn-primary" name="connexion">connexion</button>
     </form>
     </div>
 
--- include/database.php
+++ include/database.php
@@ -3,7 +3,6 @@
 //connect to database
 try{
     $pdo=new PDO("mysql:host=localhost;dbname=ecommerce_php","root","");
-    echo"connected";
 }catch(PDOException $e){
     echo "connection failed: ".$e->getMessage();
     die();
