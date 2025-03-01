2bda359decc62491910fb8c20642ddb845dcd7a0
ahmedazagaz <contactwithazagaz@gmail.com>
2025-03-01 18:24:44 +0100

    navbar

--- include/nav.php
+++ include/nav.php
@@ -8,10 +8,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Ajouter utilisateur</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Ajouter utilisateur</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Produits</a>
+          <a class="nav-link" href="connexion.php">Conexion</a>
         </li>
       </ul>
     </div>
